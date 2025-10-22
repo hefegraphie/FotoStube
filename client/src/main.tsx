cba4fa8d399d17f31fe75d1f78fd3db623e49604
hefegraphie <github@hefegraphie.de>
2025-10-22 08:33:02 +0200

    Add files via upload

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -18,12 +17,10 @@ const queryClient = new QueryClient({
 });
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Router>
         <App />
       </Router>
       <Toaster />
     </QueryClientProvider>
-  </StrictMode>
 );
